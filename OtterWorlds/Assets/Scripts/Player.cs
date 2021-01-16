e2094cb05ca470c34465affc7bcae9fca02df999
Prudchenko <javaj@ukr.net>
2021-01-16 18:12:42 +0200

    Added jump mechanic.

--- OtterWorlds/Assets/Scripts/Player.cs
+++ OtterWorlds/Assets/Scripts/Player.cs
@@ -10,7 +10,18 @@ public class Player : MonoBehaviour
     private float movementSpeed;
     private bool attack;
     private bool facingRight;
-
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float groundRadius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool jump;
+    [SerializeField]
+    private bool airControl;
+    [SerializeField]
+    private float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,22 +39,35 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        //Checks if player is grounded
+        isGrounded = IsGrounded();
+        Debug.Log(isGrounded);
+        //Moves player if possible
         HandleMovement(horizontal);
+        //Flips Player if possible
         Flip(horizontal);
+        //Attack is possible
         HandleAttack();
         ResetValues();
     }
     private void HandleMovement(float horizontal)
     {
-        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        //Moving mechanic and animation
+        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")&&(isGrounded||airControl))
         {
             myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
         }
-        //Moving mechanic and animation
+        if (isGrounded && jump)
+        {
+            Debug.Log("Jump");
+            isGrounded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+        }
         myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
     }
     private void HandleAttack()
     {
+        //Attack animation
         if (attack&& !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
         {
             myAnimator.SetTrigger("attack");
@@ -51,18 +75,23 @@ public class Player : MonoBehaviour
         }
     }
     
+    //Checks for input for special actions
     private void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             attack = true;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
     }
 
     //Flipping player when moving left-right
     private void Flip (float horizontal)
     {
-        if(horizontal>0&& !facingRight||horizontal < 0 && facingRight)
+        if((horizontal>0&& !facingRight||horizontal < 0 && facingRight)&& !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
         {
             facingRight = !facingRight;
             Vector3 theScale = transform.localScale;
@@ -73,5 +102,27 @@ public class Player : MonoBehaviour
     private void ResetValues()
     {
         attack = false;
+        jump = false;
+    }
+
+    //Checks if otter's paws touch something
+    private bool IsGrounded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach(Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for(int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
